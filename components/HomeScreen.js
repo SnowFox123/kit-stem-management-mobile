0f0bc0925e539d4e1c0cbfd33f26b58fb920f089
tungnt35 <tungntse172484@fpt.edu.vn>
2024-10-30 19:49:01 +0700

    fix: view homepage

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -8,11 +8,13 @@ import { getBlogs } from '../service/blogService';
 import BlogCard from './component/card';
 import { getAllCategories } from '../service/categoryService';
 import Tag from './component/tag';
+import { getKit } from '../service/UserServices';
 
 const HomeScreen = () => {
-    const navigation = useNavigation(); // Initialize navigation
+    const navigation = useNavigation();
     const [blogs, setBlogs] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [kit, setKit] = useState([]);
 
     const fetchCategories = async () => {
         try {
@@ -23,6 +25,30 @@ const HomeScreen = () => {
             setCategories(response);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    const fetchKits = async () => {
+        try {
+            const response = await getKit(
+                {
+                    searchCondition: {
+                        keyword: "",
+                        category_id: "",
+                        status: "",
+                        is_deleted: false
+                    },
+                    pageInfo: {
+                        pageNum: 1,
+                        pageSize: 2
+                    }
+                });
+            console.log(response);
+            setKit(response.data.pageData);
+        } catch (error) {
+            console.log('====================================');
+            console.log(error);
+            console.log('====================================');
         }
     }
 
@@ -41,6 +67,7 @@ const HomeScreen = () => {
     useEffect(() => {
         fetchBlogs();
         fetchCategories();
+        fetchKits();
     }, []);
 
     return (
@@ -68,23 +95,48 @@ const HomeScreen = () => {
             </View>
             {/* Main content goes here */}
 
-            {/* Test */}
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                {categories.length > 0 ? (
-                    categories.map((category) => (
-                        <Tag
-                            key={category._id}
-                            name={category.name}
+
+            {/* Featured Kits */}
+            <Text style={{ marginTop: 20, fontSize: 21, alignSelf: 'center', fontWeight: '600' }}>Featured STEM Kits</Text>
+            {/* Kit name, description, level, image, start, detail button */}
+            <ScrollView horizontal={true} contentContainerStyle={styles.recommendedKits} showsHorizontalScrollIndicator={false}>
+                {kit.map((kit) => (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Detailkits', { kitId: kit._id })}
+                        style={styles.card}
+                        key={kit._id}
+                    >
+                        {/* card image */}
+                        <Image
+                            source={{ uri: kit.image_url }}
+                            style={styles.cardImage}
+                            resizeMode="cover"
                         />
-                    ))
-                ) : (
-                    <Text>No Category available.</Text>
-                )}
+                        {/* end card image */}
+                        <View style={{ padding: 10 }}>
+                            {/* kit name */}
+                            <Text style={styles.cardContent} numberOfLines={2}>{kit.name}</Text>
+
+                            <View style={styles.cardRating}>
+                                <Icon name="star" size={24} color="#FFD700" />
+                                <Text style={{ fontSize: 18, fontWeight: '600', marginLeft: 6 }}>sao</Text>
+                            </View>
+                            {/* end kit name */}
+                            <View style={styles.cardCategory}>
+                                <Text style={styles.cardCategory}>{kit.category_name}</Text>
+                            </View>
+                        </View>
+
+                    </TouchableOpacity>
+                ))}
             </ScrollView>
-            {/* End Test */}
+
+            {/* End Featured Kits */}
+
+
 
             {/* Our BLogs */}
-            <Text style={{ fontSize: 30, textAlign: 'center', fontWeight: '700' }}>Our Blogs</Text>
+            <Text style={{ fontSize: 24, textAlign: 'center', fontWeight: '700' }}>Our Blogs</Text>
             <ScrollView horizontal={true} contentContainerStyle={styles.scrollViewStyle} showsHorizontalScrollIndicator={false}>
                 {blogs.length > 0 ? (
                     blogs.map((blog) => (
@@ -123,21 +175,21 @@ const styles = StyleSheet.create({
     iconContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        position: 'absolute', // Absolute positioning to move to the right
-        right: 16, // Adjust this value to position icons further or closer to the edge
+        position: 'absolute',
+        right: 16,
     },
     icon: {
-        marginLeft: 16, // Space between icons
+        marginLeft: 16,
         fontSize: 20,
     },
     logo: {
-        width: 200, // Adjust as needed
-        height: 100, // Adjust as needed
-        alignSelf: 'center', // Center logo
+        width: 200,
+        height: 100,
+        alignSelf: 'center',
         justifyContent: 'center',
-        position: 'absolute', // Positioning the logo absolutely
-        left: '50%', // Move to the center horizontally
-        transform: [{ translateX: -100 }], // Offset the logo to the left by half of its width
+        position: 'absolute',
+        left: '50%',
+        transform: [{ translateX: -100 }],
     },
     content: {
         flex: 1,
@@ -149,12 +201,46 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     scrollViewStyle: {
-        paddingHorizontal: 20,
+        paddingHorizontal: 10,
         alignItems: 'center',
         justifyContent: 'center',
         gap: 20,
-        height: 500
-    }
+        height: 400,
+    },
+    recommendedKits: {
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+    },
+    card: {
+        width: 250,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        elevation: 2,
+        marginRight: 16,
+        marginBottom: 20,
+    },
+    cardImage: {
+        width: '100%',
+        height: 150,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    cardTitle: {
+        fontSize: 16,
+        fontWeight: '600',
+        marginBottom: 6,
+    },
+    cardCategory: {
+        fontSize: 14,
+        fontWeight: '500',
+    },
+    cardRating: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
 });
 
 export default HomeScreen;
